07cc79ecea9599773bb87bb22ff1a71b72708258
Malvin King <malvintech2@gmail.com>
2025-12-15 11:00:35 +0200

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,14 @@
+//=====🇿🇼 ᴢɪᴍᴄʏʙᴇʀ  xᴅ🔥====
+
+module.exports = {
+  BOT_NAME: 'ᴢɪᴍᴄʏʙᴇʀ xᴅ', // ur bot name
+  OWNER_NAME: 'ᴍʀ sʜᴇʟʟᴀʜ ᴏғᴄ', //your name
+  OWNER_NUMBER: '263714757857', // add your phone number
+  SESSION_ID: '', // add your session ID
+  MODE: "public", // public or private
+  TIMEZONE: 'Africa/Harare' // ur timezone
+};
+
+// ᴛʜɪs ɪs ɴᴏᴛ ᴀɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ
+// ᴄʟᴏɴɪɴɢ ᴏʀ ᴍᴏᴅɪғʏɪɴɢ ᴛʜᴇ ᴄᴏᴅᴇ ᴡɪᴛʜᴏᴜᴛ ᴘᴇʀᴍɪsɪᴏɴ ᴍᴀʏ ʟᴇᴀᴅ ᴛᴏ ʟᴇɢᴀʟ ᴀᴄᴛɪᴏɴs ʙᴇɪɴɢ ᴛᴀᴋᴇɴ
+// ʀᴇɢᴀʀᴅs ᴍʀ sʜᴇʟʟᴀʜ ᴏғᴄ & ᴍʀ ᴍᴀʟᴠɪɴ ᴋɪɴɢ (xᴅᴋɪɴɢ2)
